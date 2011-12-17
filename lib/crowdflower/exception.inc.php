3530946719a16e179daeba870a5cc52309544fe7
Tom Melendez <tom@supertom.com>
2011-12-17 14:44:30 -0800

    added backtrace

--- lib/crowdflower/exception.inc.php
+++ lib/crowdflower/exception.inc.php
@@ -3,6 +3,7 @@
 class CrowdFlower_Exception {
   function __construct($msg, $method="", $line="") {
     print "EXCEPTION: Line: $line Method: $method Error: $msg";
+    debug_print_backtrace();
     exit(1);
   }
 }
